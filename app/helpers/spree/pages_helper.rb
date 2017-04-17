85deb72a983aaf2ac790424a430d807b94b0d94c
Noel <noel@2bedigital.com>
2017-04-17 09:30:44 +0100

    bug when search a page with traslations

--- app/helpers/spree/pages_helper.rb
+++ app/helpers/spree/pages_helper.rb
@@ -3,4 +3,14 @@ module Spree::PagesHelper
     page = Spree::Page.find_by_slug(slug)
     raw page.body if page
   end
+
+  #Allows to generate the correct url for each page and each locale. Call it from your views
+  def switch_language(locale)
+		page = Spree::Page.visible.with_translations.find_by_slug(request.path)
+		if page.nil?
+			url_for(:locale => locale)
+		else
+			Spree::Page::Translation.where(spree_page_id: page.id, locale: locale).first.slug
+		end
+	end
 end
--- lib/solidus_static_content.rb
+++ lib/solidus_static_content.rb
@@ -16,6 +16,6 @@ end
 class Spree::StaticPage
   def self.matches?(request)
     return false if request.path =~ /(^\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+)/
-    !Spree::Page.visible.find_by_slug(request.path).nil?
+    !Spree::Page.visible.with_translations.find_by_slug(request.path).nil?
   end
 end
